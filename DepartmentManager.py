9477bcd70679ec3e226701d783a58a92cce542f5
cxs496 <cxs496@case.edu>
2018-03-28 13:47:47 -0400

    department staff and department manager UI done

--- DepartmentManager.py
+++ DepartmentManager.py
@@ -5,6 +5,9 @@ ratings = "YOU SUCK"
 comment = ""
 respond = ""
 workinghours = 0
+decision_exists = False
+decision = "Bazinga"
+employeeID = 0000
 
 def clicked1():
     comment = txt1.get("1.0", "end-1c")
@@ -29,6 +32,23 @@ def clicked4():
 def clicked5():
     workinghours = txt5.get()
     print(workinghours)
+
+
+def clicked6():
+    if decision_exists:
+        lbl6_1.configure(text=decision)
+    else:
+        text = "No decision was made recently"
+        lbl6_1.configure(text=text)
+
+
+def clicked6_1():
+    return 0
+
+
+def clicked7():
+    text = "Employee:" + str(employeeID) + "'s working hour has been approved"
+    lbl7.configure(text=text)
 
 def get_payroll():
     return str(payroll)
@@ -81,5 +101,23 @@ txt5.grid(column=1, row=4)
 
 btn5 = Button(window, text="Set hours", command=clicked5)
 btn5.grid(column=2, row=4)
+
+lbl6 = Label(window, text="Senior Manager's Decision: ")
+lbl6.grid(column=0, row=5)
+
+lbl6_1 = Label(window, text="")
+lbl6_1.grid(column=1, row=5)
+
+btn6 = Button(window, text="Read Decision", command=clicked6)
+btn6.grid(column=2, row=5)
+
+btn6_1 = Button(window, text="Send Decision", command=clicked6_1)
+btn6_1.grid(column=3, row=5)
+
+lbl7 = Label(window, text="")
+lbl7.grid(column=1, row=6)
+
+btn7 = Button(window, text="Approve Hours", command=clicked7)
+btn7.grid(column=2, row=6)
 
 window.mainloop()
